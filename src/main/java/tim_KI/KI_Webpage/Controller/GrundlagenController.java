319d2f6ae7d751c96886a2148bb14f5445950144
timlippert <tim121202@gmail.com>
2026-02-11 08:21:43 +0100

    add DTO for maschinelles lernen and Controller aswell

--- src/main/java/tim_KI/KI_Webpage/Controller/GrundlagenController.java
+++ src/main/java/tim_KI/KI_Webpage/Controller/GrundlagenController.java
@@ -1,49 +1,48 @@
 package tim_KI.KI_Webpage.Controller;
 
+import org.springframework.web.bind.annotation.*;
 import tim_KI.KI_Webpage.DTO.Grundlagen;
 import tim_KI.KI_Webpage.Repository.GrundlagenRepository.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/grundlagen")
-    public class GrundlagenController {
-    @Autowired
-    Was_ist_KI wasIstKi;
+public class GrundlagenController {
 
-    @Autowired
-    Geschichte geschichte;
+    private final Was_ist_KI wasIstKi;
+    private final Geschichte geschichte;
+    private final Mathematische_Grundlagen mathematischeGrundlagen;
+    private final Neuronale_Netze neuronaleNetze;
 
-    @Autowired
-    Mathematische_Grundlagen mathematischeGrundlagen;
-
-    @Autowired
-    Neuronale_Netze neuronaleNetze;
-
-        public GrundlagenController(Was_ist_KI wasIstKi){
-            this.wasIstKi = wasIstKi;
-        }
-
-        @GetMapping("/was-ist-ki")
-        public Grundlagen wasIstKi() {
-            return wasIstKi.was_ist_KI();
-        }
-
-        @GetMapping("/geschichte")
-        public Grundlagen geschichte() {
-            return geschichte.geschichte();
-        }
-        @GetMapping("/mathematische-grundlagen")
-        public Grundlagen mathematische_grundlagen(){
-            return mathematischeGrundlagen.mathematische_Grundlagen();
+    public GrundlagenController(
+            Was_ist_KI wasIstKi,
+            Geschichte geschichte,
+            Mathematische_Grundlagen mathematischeGrundlagen,
+            Neuronale_Netze neuronaleNetze
+    ) {
+        this.wasIstKi = wasIstKi;
+        this.geschichte = geschichte;
+        this.mathematischeGrundlagen = mathematischeGrundlagen;
+        this.neuronaleNetze = neuronaleNetze;
     }
-        @GetMapping("/neuronaleNetze")
-        public Grundlagen neuronaleNetze()  {
-            return neuronaleNetze.neuronale_Netze();
-        }
 
+    @GetMapping("/was-ist-ki")
+    public Grundlagen wasIstKi() {
+        return wasIstKi.was_ist_KI();
+    }
+
+    @GetMapping("/geschichte")
+    public Grundlagen geschichte() {
+        return geschichte.geschichte();
+    }
+
+    @GetMapping("/mathematische-grundlagen")
+    public Grundlagen mathematischeGrundlagen() {
+        return mathematischeGrundlagen.mathematische_Grundlagen();
+    }
+
+    @GetMapping("/neuronalenetze")
+    public Grundlagen neuronaleNetze() {
+        return neuronaleNetze.neuronale_Netze();
+    }
 }
--- src/main/java/tim_KI/KI_Webpage/Controller/MaschinellesLernenController.java
+++ src/main/java/tim_KI/KI_Webpage/Controller/MaschinellesLernenController.java
@@ -1,52 +1,48 @@
 package tim_KI.KI_Webpage.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tim_KI.KI_Webpage.DTO.Grundlagen;
+import org.springframework.web.bind.annotation.*;
 import tim_KI.KI_Webpage.DTO.Maschinelles_Lernen;
-import tim_KI.KI_Webpage.Repository.Maschinenelles_Lernen.Reinforcement_Learning;
-import tim_KI.KI_Webpage.Repository.Maschinenelles_Lernen.Semi_and_Self_SLearning;
-import tim_KI.KI_Webpage.Repository.Maschinenelles_Lernen.Supervised_Learning;
-import tim_KI.KI_Webpage.Repository.Maschinenelles_Lernen.Unsupervised_Learning;
+import tim_KI.KI_Webpage.Repository.Maschinenelles_Lernen.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/maschinellese_lernen")
+@RequestMapping("/api/lernen")
 public class MaschinellesLernenController {
-    @Autowired
-    Reinforcement_Learning reinforcementLearning;
 
-    @Autowired
-    Semi_and_Self_SLearning semiAndSelfSLearning;
+    private final Reinforcement_Learning reinforcementLearning;
+    private final Semi_and_Self_SLearning semiAndSelfSLearning;
+    private final Supervised_Learning supervisedLearning;
+    private final Unsupervised_Learning unsupervisedLearning;
 
-    @Autowired
-    Supervised_Learning supervisedLearning;
+    public MaschinellesLernenController(
+            Reinforcement_Learning reinforcementLearning,
+            Semi_and_Self_SLearning semiAndSelfSLearning,
+            Supervised_Learning supervisedLearning,
+            Unsupervised_Learning unsupervisedLearning
+    ) {
+        this.reinforcementLearning = reinforcementLearning;
+        this.semiAndSelfSLearning = semiAndSelfSLearning;
+        this.supervisedLearning = supervisedLearning;
+        this.unsupervisedLearning = unsupervisedLearning;
+    }
 
-    @Autowired
-    Unsupervised_Learning unsupervisedLearning;
-
-    @GetMapping("/reinforcement_learning")
-    public Maschinelles_Lernen reinforcement_Learning() {
+    @GetMapping("/rl")
+    public Maschinelles_Lernen reinforcementLearning() {
         return reinforcementLearning.reinforcement_Learning();
     }
 
-    @GetMapping("/semi_and_selfsupervosed_learning")
-    public Maschinelles_Lernen semiAndSelfSLearning() {
+    @GetMapping("/ssl")
+    public Maschinelles_Lernen semiAndSelfLearning() {
         return semiAndSelfSLearning.semi_and_self();
     }
 
-    @GetMapping("/supervised_learning")
-    public Maschinelles_Lernen supervised_learning() {
+    @GetMapping("/sl")
+    public Maschinelles_Lernen supervisedLearning() {
         return supervisedLearning.supervised_learning();
     }
 
-    @GetMapping("/unsupervised_learning")
+    @GetMapping("/ul")
     public Maschinelles_Lernen unsupervisedLearning() {
         return unsupervisedLearning.unsupervised_Learning();
     }
 }
-
